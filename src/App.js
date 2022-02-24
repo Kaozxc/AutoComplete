71a4b2ec2fbc5df02d01eda338744ad1c8981cf4
Kaoz <kaoz415@gmail.com>
2022-02-24 19:52:41 +0100

    huge mess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import usersData from './users.json';
 
-function App() {
+const App = () => {
+  const [userInput, setUserInput] = useState();
+  const [listOfFoundUsers, setListOfFoundUsers] = useState([]);
+
+  let arr = [];
+
+  const usersFromJson = (usersData) => {
+    for(let i = 0; i < usersData.length; i++) {
+      arr += JSON.stringify(usersData[i].name);
+    }
+    arr = arr.split('"').filter(n => n !== '' );
+    return arr;
+  }
+
+
+  const handleClick = (e) => {
+    let foundUsers = [];
+    //  console.log(e.target.value)
+    for(let i = 0; i < arr.length; i++) {
+      let name = arr[i].toString();
+      console.log('name', name);
+      console.log('before', e.target.value);
+      console.log('before',arr[i][0]);
+      if(e.target.value.toLowerCase() === arr[i].substr(0, e.target.value.length).toLowerCase() ) {
+        foundUsers += `'${arr[i]}'`
+      } else {
+      console.log('In the if',e.target.value);
+      console.log('In the if',arr[i]);
+        continue;
+      }
+    }
+    setListOfFoundUsers(foundUsers.split("'").filter(n => n !== '' ));
+    console.log('found users', foundUsers.split("'").filter(n => n !== '' ));
+  }
+
+  let listComponent;
+
+  {
+    if(listOfFoundUsers.length) {
+      listComponent = (
+        <ul>
+        {console.log(listOfFoundUsers)}
+        {console.log('typeof', typeof listOfFoundUsers)}
+          {listOfFoundUsers.map((listOfFoundUsers, i) => {
+            return (
+              <li key={3} onClick={handleClick}>
+              {listOfFoundUsers}
+            </li>
+            );
+          })}
+        </ul>
+      )  
+    } 
+  }
+{}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <form>
+         <input type="text" onChange={e => handleClick(e.target.value)} placeholder="Users"/>
+          <button type='submit' onClick={submitForm}>Button</button>
+      </form> */}
+      <input type="text" onChange={e => handleClick(e)} placeholder="Users"/> 
+        {listComponent}
+      <br/>
+
+     Users : {usersFromJson(usersData)}
     </div>
-  );
+  )
 }
 
 export default App;
